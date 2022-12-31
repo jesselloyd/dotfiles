671fcb76a54ba11ea0f4613a03cef589e61ddb95
Jesse Lloyd <jesse.a.lloyd@gmail.com>
2022-12-31 12:39:42 -0600

    feat: remap updates, telescope settings

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,11 +1,11 @@
 vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<C-n>", vim.cmd.Lex)
+vim.keymap.set("n", "<leader>n", vim.cmd.Lex)
 
 vim.keymap.set("x", "<S-p>", [["_dP]])
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
