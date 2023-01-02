07703aba2673ac286d5af02200429e4d4693cf56
Jesse Lloyd <jesse.a.lloyd@gmail.com>
2023-01-02 09:13:08 -0600

    feat: update remaps

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,9 +1,15 @@
 vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<leader>n", vim.cmd.Lex, { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<C-n>", vim.cmd.Lex, { desc = "Toggle file explorer" })
 
 vim.keymap.set("x", "<S-p>", [["_dP]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<S-y>", [["+Y]])
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
