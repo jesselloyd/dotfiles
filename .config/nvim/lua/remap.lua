bea1c9e1e84aef5c12881a663cbed929b9fe5e75
Jesse Lloyd <jesse.a.lloyd@gmail.com>
2023-01-02 09:26:07 -0600

    fix: yank/paste remaps

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<C-n>", vim.cmd.Lex, { desc = "Toggle file explorer" })
 
-vim.keymap.set("x", "<S-p>", [["_dP]])
-
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<S-y>", [["+Y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
