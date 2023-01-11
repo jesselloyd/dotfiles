df5a8ccc2a1ddd4c36c4179a12733e51f375d060
Jesse Lloyd <jesse.a.lloyd@gmail.com>
2023-01-11 09:59:22 -0600

    fix: eslint lsp auto fix settings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -9,6 +9,17 @@ lsp.ensure_installed({
 	"rust_analyzer",
 })
 
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({ mapping = cmp_mappings })
+
 -- Fix undefined global 'vim'
 lsp.configure("sumneko_lua", {
 	settings = {
@@ -20,9 +31,16 @@ lsp.configure("sumneko_lua", {
 	},
 })
 
-local config = require("lspconfig")
+lsp.configure("eslint", {
+	settings = {
+		autoFixOnSave = true,
+	},
+	codeActionsOnSave = {
+		mode = "all",
+	},
+})
 
-config.tailwindcss.setup({
+lsp.configure("tailwindcss", {
 	settings = {
 		editor = {
 			quickSuggestions = { strings = true },
